3e4947a71561a00d28aee47205b9dbad9542e47d
Mostafa Shams <mosstafashamss@gmail.com>
2023-05-17 00:40:38 +0300

    setting up environment

---
+++ src/main/java/com/example/restServiceETA/RestServiceEtaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restServiceETA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceEtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceEtaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/restServiceETA/controller/ETAController.java
@@ -0,0 +1,14 @@
+package com.example.restServiceETA.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class ETAController
+{
+
+}
+
+
+
